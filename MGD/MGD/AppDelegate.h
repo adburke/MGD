1c17ede89d1cfee0d97bfa308df6301cbad856c3
adburke <adburke@fullsail.edu>
2014-05-08 11:54:16 -0500

    Initial Creation

---
+++ MGD/MGD/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MGD
+//
+//  Created by Aaron Burke on 5/7/14.
+//  Copyright (c) 2014 Aaron Burke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MGD/MGD/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  MGD
+//
+
+//  Copyright (c) 2014 Aaron Burke. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
